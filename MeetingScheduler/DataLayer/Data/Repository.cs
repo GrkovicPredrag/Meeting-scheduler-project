6a10f6a9b965eb7a4ee7161009205e4d847820d4
Predrag <garderp.civokrg@gmail.com>
2021-07-22 00:13:26 +0200

    Created the DB repository

---
+++ MeetingScheduler/DataLayer/Data/Repository.cs
@@ -0,0 +1,78 @@
+﻿using DataLayer.Interfaces;
+using Logger;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Data
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private SchedulerContext _context = null;
+        private DbSet<TEntity> _table = null;
+        private ILogger _logger = null;
+
+        public Repository()
+        {
+            _context = new SchedulerContext();
+            _table = _context.Set<TEntity>();
+            _logger = LoggerFactory.Create(LoggerFactory.LoggingOption.Output);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            IEnumerable<TEntity> toReturn = null;
+            try
+            {
+                toReturn = _table.ToList();
+                _logger.Log("Succesfully returned objects! [GetAll]");
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(ex.Message.ToString());
+            }
+            return toReturn;
+        }
+
+        public void Insert(TEntity entity)
+        {
+            try
+            {
+                _table.Add(entity);
+                _context.SaveChanges();
+                _logger.Log("Successfully added entity. [Insert]");
+            }
+            catch (DbEntityValidationException ex)
+            {
+                foreach (var errors in ex.EntityValidationErrors)
+                {
+                    foreach (var validationError in errors.ValidationErrors)
+                    {
+                        // get the error message 
+                        string errorMessage = validationError.ErrorMessage;
+                        _logger.Log(errorMessage);
+                    }
+                }
+            }
+        }
+
+        public void Delete(object id)
+        {
+
+        }
+
+        public void Update(TEntity entity)
+        {
+
+        }
+
+        public TEntity GetByID(object id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MeetingScheduler/TestApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using DataLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository<User> _userRepo = new Repository<User>();
+            Repository<Team> _teamRepo = new Repository<Team>();
+
+            User user = new User();
+            user.Username = "smema91";
+            user.Firstname = "Natalija";
+            user.Lastname = "Nikoletic";
+            user.Password = "smema05";
+            user.Role = "employee";
+            user.Email = "natalija.nikoletic.yahoo.com";
+            user.TeamId = 1;
+
+            Team team = new Team();
+            team.Name = "Cool Guys Inc.";
+            team.Desc = "Best team around";
+
+            //_teamRepo.Insert(team);
+            //_userRepo.Insert(user);
+
+            //IEnumerable<Team> teams = _teamRepo.GetAll();
+            IEnumerable<User> users = _userRepo.GetAll();
+        }
+    }
+}
